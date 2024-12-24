2530df01c97948f4006cbd43d0d77daf2f91623c
CrispysCode <pr0ject0dinx@gmail.com>
2024-12-23 23:11:59 -0600

    completed exercise 07 using a variable and Math.round()

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  let fToC = (num - 32) / 1.8;
+  return Math.round(fToC * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(vnum) {
+  let cToF = vnum * 1.8 + 32;
+  return Math.round(cToF * 10) / 10;
 };
 
 // Do not edit below this line
