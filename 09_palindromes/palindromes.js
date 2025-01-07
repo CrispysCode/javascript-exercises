c94a708e388a21a82b6496dc965e90cb6f6189fd
CrispysCode <pr0ject0dinx@gmail.com>
2025-01-07 14:08:18 -0600

    completed exercise 09 w/ two different syntax

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,31 @@
-const palindromes = function () {
+ const palindromes = function (num) {
+ let regExp = /[\W_]/g; 
 
+  //Lowercase string & combine so there's no whitespace
+ let lowRegStr = num.toLowerCase().replace(regExp, '');
+
+  //Stores the reversed lowRegStr in a variable to check if palindrome
+ let reverseStr = lowRegStr.split('').reverse().join('');
+
+ return reverseStr === lowRegStr;
 };
 
+/* diff syntax 
+
+function palindromes(str) {
+  let regExp = /[^A-Za-z0-9]/g;
+
+  str = str.toLowerCase().replace(regExp, '');
+
+  let len = str.length;
+
+  for (let i = 0; i < len / 2; i++) {
+    if (str[i] !== str[len - 1 - i]) {
+      return false; 
+    }
+  }
+    return true;
+}
+*/
 // Do not edit below this line
 module.exports = palindromes;
