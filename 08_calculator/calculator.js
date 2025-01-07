51a547558260581e9892743b3a7ad008fe73b8b6
CrispysCode <pr0ject0dinx@gmail.com>
2025-01-07 13:47:22 -0600

    completed exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -3,23 +3,29 @@ const add = function(a, b) {
 	
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+return array.reduce((accumulator, currVal) => accumulator + currVal, 0)
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  return arr.reduce((accumulator, currVal) => accumulator * currVal);
 
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b)
 };
 
-const factorial = function() {
+const factorial = function(num) {
+   if (num === 0 || num === 1) {
+    return 1;
+  } else {
+    return num * factorial(num - 1);
+  }
 	
 };
 
