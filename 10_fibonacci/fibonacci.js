48f5b85b13d63ed8b355aef433cbed74d6990544
CrispysCode <pr0ject0dinx@gmail.com>
2025-01-07 14:38:43 -0600

    Completed exercise 11 (come back for reference)

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(countArg) {
+  let count;
+  let fibArr = [0, 1];
+
+  //Checks for negative numbers!
+    if (typeof countArg !== 'number') {
+        count = parseInt(countArg)
+    } else {
+        count = countArg;
+    }
+
+  //Return expected results
+  if (count < 0) return "OOPS";
+  if (count == 0) return 0;
+
+  for (let i = 2; i <= count; i++) {
+    fibArr[i] = fibArr[i - 1] + fibArr[i - 2];
+  }
+  
+  return fibArr[count];
 
 };
 
